79a8a5d1c6253f29d15ce6fb8086f2509ff50d9a
Prince Hassan <prince-73ssan@users.noreply.github.com>
2017-08-29 15:53:24 +0400

    Working chatUI & Calendar
    In this version the work is all about UI, so the keyboard has been fixed in Andriod where previsoly it is was not rising the input. The calendar works well but I am concerend about the perfomance. I hope after exporting it as a standalone app it may perform better!

---
+++ App.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  AppRegistry,
+  StyleSheet,
+  Button,
+  KeyboardAvoidingView 
+} from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+class HomeScreen extends React.Component{
+  static navigationOptions = {
+    title: 'CHATS',
+  };
+
+  state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello NIS',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Hassan',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
+          name: 'test'
+        }}
+      />
+    );
+  }
+
+}
+
+class CalenderScreen extends React.Component{
+  static navigationOptions = {
+    title: 'CALENDER',
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {}
+    };
+  }
+
+  render() {
+    return (
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-08-26'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
+    );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'SS Test on ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
+
+class PaperScreen extends React.Component{
+  static navigationOptions = {
+    title: 'PAPERS',
+  };
+  render(){
+    return(
+      <View>
+        <Text>Chat with me!</Text>
+      </View>
+    )
+  }
+}
+
+class ProfileScreen extends React.Component{
+  static navigationOptions = {
+    title: 'PROFLE',
+  };
+  render(){
+    return(
+      <View>
+        <Text>This is users profle!</Text>
+      </View>
+    )
+  }
+}
+
+
+export default MainScreenNavigator = TabNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: CalenderScreen },
+  Paper: { screen: PaperScreen},
+  Profile: { screen: ProfileScreen},
+});
